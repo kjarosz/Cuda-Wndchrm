abe4a7f8bb393f54d5128f273a0b81028a17587c
Kamil Jarosz <kjarosz19946@gmail.com>
2016-05-05 12:16:12 -0400

    Separate cuda signatures to their own file, away from the container.

---
+++ udat_on_cuda/cuda_signatures.h
@@ -0,0 +1,75 @@
+#ifndef CUDASIGNATURES_H
+#define CUDASIGNATURES_H
+
+
+
+#include "image_matrix.h"
+#include "file_manip.h"
+#include "signatures.h"
+#include "constants.h"
+
+
+
+struct DirectoryTracker
+{
+  char **directories;
+  int count;
+  int current_dir;
+  char *directory;
+  DIR *opened_dir;
+};
+
+
+
+class CUDASignatures
+{
+public:
+  CUDASignatures();
+  ~CUDASignatures();
+
+  void compute(char **directories, int count);
+
+  void save_in(char *directory);
+
+
+
+private:
+  CUDASignatures(const CUDASignatures &source);
+  CUDASignatures &operator=(const CUDASignatures &other);
+
+
+
+private:
+  bool supported_format(char *filename);
+
+  void reset_directory_tracker(char **directories, int count);
+  bool read_next_batch();
+  bool batch_capacity_reached();
+  dirent *read_next_entry();
+
+  void load_image_matrix(char *filename);
+  void double_matrix_container();
+  void empty_matrix_container();
+  
+  void compute_signatures_on_cuda();
+
+  // All CUDA functions.
+  void compute_zernike_on_cuda(pix_data **images, int *widths, int *heights, int *depths, double *outputs, long *sizes);
+  
+  void allocate_signature_buffers();
+  void add_signatures(double *output, long size);
+  void deallocate_signature_buffers();
+
+private:
+  DirectoryTracker directory_tracker;
+  
+  ImageMatrix **image_matrices;
+  int matrix_container_size;
+  int image_matrix_count;
+
+  Signatures signatures;
+};
+
+
+
+#endif // CUDASIGNATURES_H
--- udat_on_cuda/signatures.h
+++ udat_on_cuda/signatures.h
@@ -5,32 +5,10 @@
 
 #include <string>
 #include <vector>
-#include <map>
 
 
 
-#include "image_matrix.h"
-#include "file_manip.h"
-
-
-
-const int INIT_MATRIX_CONTAINER_SIZE = 16;
-const int INIT_SIG_CONTAINER_SIZE    = 64;
-
-const int SIGNATURE_NAME_LENGTH      = 80;
-
-const int MAX_OUTPUT_SIZE            = 72;
-
-
-
-struct DirectoryTracker
-{
-  char **directories;
-  int count;
-  int current_dir;
-  char *directory;
-  DIR *opened_dir;
-};
+#include "constants.h"
 
 
 
@@ -89,57 +67,6 @@ private:
   char **sigs;    // Columns
   char **files;   // Rows
   double *values; // Data matrix
-};
-
-
-
-class CUDASignatures
-{
-public:
-  CUDASignatures();
-  ~CUDASignatures();
-
-  void compute(char **directories, int count);
-
-  void save_in(char *directory);
-
-
-
-private:
-  CUDASignatures(const CUDASignatures &source);
-  CUDASignatures &operator=(const CUDASignatures &other);
-
-
-
-private:
-  bool supported_format(char *filename);
-
-  void reset_directory_tracker(char **directories, int count);
-  bool read_next_batch();
-  bool batch_capacity_reached();
-  dirent *read_next_entry();
-
-  void load_image_matrix(char *filename);
-  void double_matrix_container();
-  void empty_matrix_container();
-  
-  void compute_signatures_on_cuda();
-
-  // All CUDA functions.
-  void compute_zernike_on_cuda(pix_data **images, int *widths, int *heights, int *depths, double *outputs, long *sizes);
-  
-  void allocate_signature_buffers();
-  void add_signatures(double *output, long size);
-  void deallocate_signature_buffers();
-
-private:
-  DirectoryTracker directory_tracker;
-  
-  ImageMatrix **image_matrices;
-  int matrix_container_size;
-  int image_matrix_count;
-
-  Signatures signatures;
 };
 
 
